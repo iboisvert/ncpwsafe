65515b17d4400c2c9765e5f9b7b89e88a031bdf9
Ian Boisvert <20719482+iboisvert@users.noreply.github.com>
2023-08-23 08:33:26 +0000

    Fix export db

--- src/ExportDbCommand.cpp
+++ src/ExportDbCommand.cpp
@@ -26,26 +26,14 @@ int ExportDbCommand::Execute()
     std::unique_ptr<FILE, decltype(&fclose)> pf{f, &fclose};
 
     // Header
-    fprintf(f, "UUID, Group, Title, User, Password, Notes\n");
+    fprintf(f, "UUID,Group,Title,User,Password,Notes\n");
     // Records
     auto &records = db.Records();
     for (const AccountRecord &rec : records)
     {
-        const std::string &uuid = rec.GetField(FT_UUID);
-        std::string suuid;
-        char buf[3];
-        for (size_t i = 0; i < 16; ++i)
-        {
-            if (i == 4 || i == 6 || i == 8 || i == 10)
-                suuid.append("-");
-            sprintf(buf, "%02x", uuid[i]);
-            suuid.append(buf);
-        }
-        std::string notes = rec.GetField(FT_NOTES);
-        notes = std::string{rtrim(notes.begin(), notes.end()), notes.end()};
-        fprintf(f, "%s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\"\n",
-                suuid.c_str(), rec.GetField(FT_GROUP), rec.GetField(FT_TITLE),
-                rec.GetField(FT_USER), rec.GetField(FT_PASSWORD), notes.c_str());
+        fprintf(f, "%s,\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n",
+                rec.GetField(FT_UUID, ""), rec.GetField(FT_GROUP, ""), rec.GetField(FT_TITLE, ""),
+                rec.GetField(FT_USER, ""), rec.GetField(FT_PASSWORD, ""), rec.GetField(FT_NOTES, ""));
     }
 
     return RC_SUCCESS;
